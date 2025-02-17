f07089164b6e4d65dbcee7cbeb3617b0d434976b
dydfuf <88dydfuf@naver.com>
2025-02-18 00:23:00 +0900

    아이콘을 추가합니다.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Raon.c Dev";
+export const SITE_SHORT_TITLE = "Raon.c";
 export const SITE_DESCRIPTION = 'Welcome to my website!';
